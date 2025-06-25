e25d9426d082ff4d3a93bf890a8a015d3b7d7d00
EduardaAraujo474810 <eduarda.gabrielle.araujo@escola.pr.gov.br>
2025-06-25 12:18:33 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,19 @@
+const perguntas = [
+     {
+        enunciado:"Como você lida com o estresse?"
+        alternativas:[
+          
+              {
+                texto: "a)     ",
+                afirmação: ""
+            }
+              {
+                texto: "a)     ",
+                afirmação: ""
+            }
+        ]
+    },
+    {
+]
+    
+      
